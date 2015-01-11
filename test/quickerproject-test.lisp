b5e2e2a86f39dc3eab86eae311c99d3daba7405b
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-01-11 12:47:11 -0800

    easter egg

--- test/quickerproject-test.lisp
+++ test/quickerproject-test.lisp
@@ -6,6 +6,8 @@
   (make-project "quickerproject-test-project")
   (ql:quickload :quickerproject-test-project)
   (ql:quickload :quickerproject-test-project-test)
+  
+  ;; https://i.imgflip.com/g9x6a.jpg
   (eval (read-from-string "(quickerproject-test-project-test:test-all)"))
   (is t)
   (format t "~&Quickerproject successfully generated a testable project.~%"))
