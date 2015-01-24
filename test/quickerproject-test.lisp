ac4accbf65191f6f6b03f6b4bff03f37fa8c376b
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-01-24 09:11:07 -0800

    oops

--- test/quickerproject-test.lisp
+++ test/quickerproject-test.lisp
@@ -5,7 +5,7 @@
 (deftest test-quickerproject ()
   (make-project "quickerproject-test-project")
   (ql:quickload "quickerproject-test-project")
-  (ql:quickload "quickerproject-test-project")
+  (ql:quickload "quickerproject-test-project-test")
   
   ;; https://i.imgflip.com/g9x6a.jpg
   (eval (read-from-string "(quickerproject-test-project-test:test-all)"))
